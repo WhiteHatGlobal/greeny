5d684312eab3b743f760324e1b185100cd09263a
RK <rk@whitehatglobal.org>
2022-12-04 11:18:27 +0530

    purchase advice

---
+++ greeny/greeny_medows/doctype/purchase_advice/test_purchase_advice.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, White Hat Global and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPurchaseAdvice(FrappeTestCase):
+	pass
